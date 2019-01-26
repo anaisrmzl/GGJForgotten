568e3d20560a744595857b5e728728c5bab434eb
Totopojr <46763040+Totopojr@users.noreply.github.com>
2019-01-25 20:35:17 -0600

    Cambio de nivel visual

---
+++ Assets/Scripts/VisualChangeLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualChangeLevel : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject MaskObject;
+    bool ChangeVisualLevel = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            ChangeVisualLevel = !ChangeVisualLevel;
+            MaskObject.SetActive(ChangeVisualLevel);
+        }
+    }
+}
